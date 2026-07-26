2c448cae724db6d92606ede83a751dc864c5305d
Anokhi <139269672+Anokhi-hirsch@users.noreply.github.com>
2026-07-26 15:39:40 -0700

    Update silver_lookup_tables.py

--- notebooks/silver_lookup_tables.py
+++ notebooks/silver_lookup_tables.py
@@ -1,1 +1,24 @@
 
+# Databricks Notebook: Lookup Silver Files
+# This notebook creates a list of lookup/reference files
+# that will be passed dynamically to the Silver notebook.
+
+files = [
+    {
+        "sourcefolder": "customers",
+        "targetfolder": "customers"
+    },
+    {
+        "sourcefolder": "products",
+        "targetfolder": "products"
+    },
+    {
+        "sourcefolder": "sellers",
+        "targetfolder": "sellers"
+    }
+]
+
+dbutils.jobs.taskValues.set(
+    key="silver_lookup_files",
+    value=files
+)
